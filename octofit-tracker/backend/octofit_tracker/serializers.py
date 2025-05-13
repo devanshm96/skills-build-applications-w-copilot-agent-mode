0b73231270a7a935b067c73516ecf20672a1d3a2
Devansh Mudgal <72990407+devanshm96@users.noreply.github.com>
2025-05-13 17:21:24 +0000

    Added relevant backend files to the project

---
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+# Define serializers for users, teams, activity, leaderboard, and workouts
+class UserSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    email = serializers.EmailField()
+    name = serializers.CharField()
+
+class TeamSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    name = serializers.CharField()
+    members = serializers.ListField()
+
+class ActivitySerializer(serializers.Serializer):
+    id = serializers.CharField()
+    user_id = serializers.CharField()
+    activity_type = serializers.CharField()
+    duration = serializers.IntegerField()
+
+class LeaderboardSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    user_id = serializers.CharField()
+    score = serializers.IntegerField()
+
+class WorkoutSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    name = serializers.CharField()
+    description = serializers.CharField()
